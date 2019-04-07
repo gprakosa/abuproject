6e860f2a75396f9cf2b735c58fe09ae22646efd0
gprakosa <godhot.prakosa@gmail.com>
2019-04-08 02:15:07 +0800

    Starter

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+  	brand: 'ABU',
+  	about: 'PROJECT',
+  	title: 'ABU Project', 
+  	person1: 'ABU SHAKEIL',
+  	person2: 'ABU GHAIDA',
+  	tagline: 'DELIVER SOLUTION',
+  	message: 'We will help your problem with a solution that meet with the original of syariat Islam, that means our solution is based on Al Quran and Assunnah (Al Hadist). Choose us to be your Ikhtiar and let\'s we tawakal together',
+  });
 });
 
 module.exports = router;
